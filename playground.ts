b88897ba60b6f9a9abc72546d9c711d45ad037d8
Sunny Golovine <sunny@glvn.co>
2023-02-22 16:04:15 -0700

    add files

---
+++ playground.ts
@@ -0,0 +1,8 @@
+// Clear the screen before printing any output.
+// Comment out this line if you want to preserve logs between runs.
+console.clear()
+
+/**
+ * Write your code here!!
+ */
+console.log('Welcome to the JS playground')
